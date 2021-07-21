3adf3bbde79e4cc8d02a35d0830cd4f69ad88a38
AntarcticRuler <34521078+AntarcticRuler@users.noreply.github.com>
2021-07-20 20:03:34 -0700

    Update #1 for 2021 Hackathon
    - Overhauled entire system. It was really meh before. Not as bad as I remembered, but just meh.
    - Created a roles & channels system instead of constantly calling by IDs. This way you can add and call any channel by simply using add_channel("name") and then channels["name"]
    - Added more to the welcome message, although I should really test that now that I think of it
    - Fixed bug with creating new team channels where it wouldn't set permissions properly
    - Created a command for deleting team channels (doesn't delete roles yet)
    - Neatened up and fixed up the !set & !create & !teams commands
    - Probably more
    
    TODO
    - Comments
    - Deleting Roles

--- index.js
+++ index.js
@@ -5,42 +5,95 @@ var client = new Discord.Client();
 
 var config = require ('./config.json');
 
-var phGeneral;
-var phInfo, ph2020;
-var phRole;
+var channels = {
+
+}
+
+function add_channel (name) {
+    channels[name] = client.channels.cache.filter(ch => ch.name === name);
+}
+
+function add_channels (names) {
+    names.forEach(name => add_channel(name))
+}
+
+var roles = {
+
+}
+
+function add_role (name) {
+    roles[name] = client.guilds.cache.array()[0].roles.cache.filter(r => r.name == name)
+}
+
+function add_roles (names) {
+    names.forEach(name => add_role(name))
+}
+
+var invalid_roles = ["participant", "organizer", "main organizer", "sponsor", "administrator", "@everyone", "peddiehacks", "new role"]
+
+var team_channels = []
 
 client.on ('ready', () => { 
-    phGeneral = client.channels.cache.get ('677158741195358252');
-    phInfo = client.channels.cache.get ('677286134862315551')
-    ph2020 = client.channels.cache.get ('677159874685304842')
-    phRole = client.guilds.cache.get("677158741195358248").roles.cache.get("677179665261133828")
 
-    client.user.setActivity("The Peddie Hackathon!", { type: "WATCHING" })
+    add_channels([
+        "announcements",
+        "shedule",
+        "judging",
+        "workshops",
+        "prizes",
+        "rules",
+        "logs"
+    ]);
+    
+    add_roles ([
+        "participant"
+    ])
+
+    let team_roles = []
+    let g = client.guilds.cache.array()[0]
+    g.roles.cache.array().forEach(r => {
+        if (invalid_roles.indexOf(r.name.toLowerCase()) == -1)
+            team_roles.push(r.name);
+    })
+    add_roles (team_roles)
+
+    g.channels.cache.array().forEach(ch => {
+        if (ch.children != undefined) {
+            if (ch.children.filter(child => child.name == "chat").array().length > 0) {
+                team_channels.push(ch)
+            } 
+        }
+    })
+
+    client.user.setActivity("The Peddie Hacks 2021!", { type: "WATCHING" })
+
+    console.log (`PEDDIE HACKS BOT RUNNING UNDER ${client.user.tag}`);
 })
 
 client.on ('guildMemberAdd', (member) => {
-    phGeneral.send (`Welcome, ${member}, please read ${phInfo} & ${ph2020}, and change your nickame to your first name plus the first letter of your last name.`);
-    member.roles.add(phRole)
+    channels['logs'].send (`Welcome, ${member}, please read ${channels["rules"]}. \nThen, change your nickame to your first name plus the first letter of your last name.\nFor the most up to date info, check out https://peddiehacks.peddie.org/`);
+    member.roles.add(roles["participant"]);
 })
 
-// This is possibly the worst discord.js code I have ever written ;-;
-// Like, no joke, this is a pile of spaghetti meant to sustain, not to taste like a 5 star cuisine
-// RXNS is an at least 4.5 star cuisine imo, with web dashboard and all but this??
-// This s u c k s.
 client.on ('message', (message) => {
-    if (message.channel.id == "742511398382338048" && message.content.toLowerCase().startsWith("!teams")) {
+    // Displays all existing teams
+    if (message.content.toLowerCase().startsWith("!teams")) {
         var roleString = "";
+        // Goes through all of a servers roles and checks adds them to a string unless they are in the invalid_roles list
         message.guild.roles.cache.array().forEach (role => { 
-            if (role.name != "Participant" && role.name != "Organizer" && role.name != "Sponsor" && role.name != "Administrator" && role.name != "@everyone" && role.name != "PeddieHacks")
+            if (invalid_roles.indexOf(role.name.toLowerCase()) == -1)
                 roleString += `* ${role.name}\n`
         })
         roleString += "\nUse !set [team name] to add your team"
-        console.log (roleString)
-        message.channel.send (new Discord.MessageEmbed().setAuthor("ROLES", client.user.avatarURL()).setDescription(roleString))
+        // console.log (roleString)
+        let embed = new Discord.MessageEmbed().setAuthor("ROLES", client.user.avatarURL()).setDescription(roleString)
+        message.channel.send (embed)
     }
-    else if (message.channel.id == "742511398382338048" && message.content.toLowerCase().startsWith("!set")) {
+
+    else if (message.content.toLowerCase().startsWith("!set")) {
         var name = message.content.toLowerCase().split(" ").splice(1).join(" ")
-        if (name == "participant" || name == "organizer" || name == "sponsor" || name == "administrator" || name == "peddiehacks")
+
+        if (roles[name] == undefined || invalid_roles.indexOf(roles[name].array()[0].name.toLowerCase()) != -1)
             return message.channel.send ("That role cannot be assigned.");
         let found = false
         message.guild.roles.cache.array().forEach (role => { 
@@ -49,15 +102,16 @@ client.on ('message', (message) => {
                     found = true
                     return message.channel.send ("You already have the role!")
                 }
-                message.member.roles.add(role).then( () =>  message.channel.send(`"${name}" role added!`))
-                found = true
+                return message.member.roles.add(role).then( () =>  message.channel.send(`"${name}" role added! \nIf you are not a member of this team, or added the role by mistake, please contact an organizer.`))
+                // found = true
             }
         })
         if (!found)
             message.channel.send ("Role not found!")
     }
+
     // Creates a role specific channel for a team
-    else if (message.member.permissions.has("ADMINISTRATOR") && message.content.toLowerCase().startsWith("!create ")) {
+    else if (message.member.permissions.has("ADMINISTRATOR") && message.content.toLowerCase().startsWith("!create ")) { // Create has a space to make sure it has two args
         var args = message.content.toLowerCase().split(" ").slice(1)
         // Creates the role
         message.guild.roles.create({
@@ -73,6 +127,7 @@ client.on ('message', (message) => {
                 }
             )
             .then(channel => {
+
                 // Overwrites permissions in the category so only the role can see
                 channel.overwritePermissions([{
                     id: role.id,
@@ -81,20 +136,42 @@ client.on ('message', (message) => {
                 {
                     id: message.guild.roles.everyone.id,
                     deny: ["VIEW_CHANNEL"]
-                }])
-                // Creates chat & voice
-                message.guild.channels.create("chat", {
-                    type: "text",
-                    parent: channel.id  
+                }]).then (() => {
+                    // Creates chat & voice
+                    message.guild.channels.create("chat", {
+                        type: "text",
+                        parent: channel.id  
+                    }).then (ch => ch.lockPermissions())
+                    message.guild.channels.create("voice", {
+                        type: "voice",
+                        parent: channel.id  
+                    }).then (ch => ch.lockPermissions())
+                    team_channels.push(channel);
                 })
-                message.guild.channels.create("voice", {
-                    type: "voice",
-                    parent: channel.id  
-                })
-                message.channel.send (`"${args.slice(" ")}" team created!`)
+
+                message.channel.send (`"${args.join(" ")}" team created!`)
+
             })
 
         })
+
+        add_role(args.join(" "));
+    }
+
+    else if (message.member.permissions.has("ADMINISTRATOR") && message.content.toLowerCase().startsWith("!delete ")) { // Create has a space to make sure it has two args
+        var name = message.content.toLowerCase().split(" ").splice(1).join(" ")
+
+        team_channels.forEach(channel => {
+            if (channel.name == name) {
+                channel.children.array().forEach (child => {
+                    child.delete()
+                })
+                channel.delete();
+                return message.channel.send(`"${name}" DELETED`)
+            }
+        });
+        
+        return message.channel.send(`"${name}" NOT FOUND`)
     }
 })
 
